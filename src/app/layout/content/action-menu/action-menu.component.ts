24e864fefc7da4aaae2d93e9955d28c76fd77b8d
smileboyi <15215212143@163.com>
2021-08-21 14:48:06 +0800

    feat: gitter反馈

--- src/app/layout/content/action-menu/action-menu.component.ts
+++ src/app/layout/content/action-menu/action-menu.component.ts
@@ -1,16 +1,44 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Output,
+  OnDestroy,
+  AfterViewInit,
+  EventEmitter,
+} from '@angular/core';
+import Gitter from 'gitter-sidecar';
 
 @Component({
   selector: 'cat-action-menu',
   templateUrl: './action-menu.component.html',
   styleUrls: ['./action-menu.component.less'],
 })
-export class ActionMenuComponent implements OnInit {
+export class ActionMenuComponent implements OnInit, AfterViewInit, OnDestroy {
   musicHidden = true;
+  chat: {
+    toggleChat: (b: boolean) => void;
+    destroy: () => void;
+  };
 
   @Output() toggleSkin = new EventEmitter<any>();
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    this.chat = new Gitter({
+      room: 'ng-mamafish/ng-mamafish',
+      activationElement: false
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.chat.toggleChat(false);
+    this.chat.destroy();
+  }
+
+  openFeedback(): void {
+    this.chat.toggleChat(true);
+  }
 }
--- src/app/typings.d.ts
+++ src/app/typings.d.ts
@@ -1,8 +1,7 @@
-declare module 'screenfull' {
+declare module screenfull {
   export const isEnabled: boolean;
   export const toggle: () => any;
 }
-
 
 declare module 'quill' {
   type QuillType = any;
@@ -12,4 +11,13 @@ declare module 'quill' {
 type QuillType = any;
 type Delta = any;
 
-
+declare module 'gitter-sidecar' {
+  interface Gitter {
+    new (o: any): {
+      toggleChat: (b: boolean) => void;
+      destroy: () => void;
+    };
+  }
+  const Gitter: Gitter;
+  export default Gitter;
+}
