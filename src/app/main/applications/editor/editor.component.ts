f402efc54e0527f61e9dd3a915655a16f11c3744
smileboyi <15215212143@163.com>
2021-12-13 00:12:44 +0800

    feat: markdown编辑器

--- src/app/main/applications/editor/editor.component.ts
+++ src/app/main/applications/editor/editor.component.ts
@@ -4,6 +4,8 @@ import {
   Input,
   ChangeDetectionStrategy,
 } from '@angular/core';
+import monaco from 'monaco-editor';
+import { marked } from 'marked';
 
 @Component({
   selector: 'cat-editor',
@@ -12,22 +14,63 @@ import {
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EditorComponent implements OnInit {
-  editorOptions = { theme: 'vs-dark', language: 'javascript' };
-  code = 'function x() {\nconsole.log("Hello world!");\n}';
-
-  @Input() template: string | null =
-    'function x() {\nconsole.log("Hello world!");\n}';
-  @Input() options = {
-    theme: 'vs-dark',
-    language: 'javascript',
+  isFullScreen = false;
+  template: string = 'function x() {\nconsole.log("Hello world!");\n}';
+  options = {
+    theme: 'vs',
+    language: 'markdown',
     automaticLayout: true,
     readOnly: false,
     fontSize: 16,
   };
 
-  constructor() {}
+  constructor() {
+
+  }
 
   ngOnInit(): void {
-    console.log()
+    console.log();
   }
+
+  handleBold(): void {}
+
+  handleItalic(): void {}
+
+  handleStrike(): void {}
+
+  handleUnderline(): void {}
+
+  handleLink(): void {}
+
+  handleOrderedlist(): void {}
+
+  handleUnorderedlist(): void {}
+
+  handleImage(): void {}
+
+  handleTable(): void {}
+
+  handleCode(): void {}
+
+  handleBlockquote(): void {}
+
+  handleFormat(): void {}
+
+  handleExpression(): void {
+    window.open('https://emojixd.com/', '_blank');
+  }
+
+  handleFullscreen(): void {
+    this.isFullScreen = true;
+  }
+
+  handleExitFullscreen(): void {
+    this.isFullScreen = false;
+  }
+
+  handlePreview(): void {}
+
+  handlePrint(): void {}
+
+  handleUpload(): void {}
 }
